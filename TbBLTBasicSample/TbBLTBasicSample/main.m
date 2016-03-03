5e3fb3b6a04c2d19c350dca20f4881233a34a4e8
tkfmueda <tkfm@3bitter.com>
2016-03-03 21:19:49 +0900

    Initial commit

---
+++ TbBLTBasicSample/TbBLTBasicSample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TbBLTBasicSample
+//
+//  Created by Ueda on 2016/03/02.
+//  Copyright © 2016年 3bitter Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
