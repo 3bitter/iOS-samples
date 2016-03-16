6bb385c50ce8b64b92aa04f4c963fba5f795c955
tkfmueda <tkfm@3bitter.com>
2016-03-16 15:31:57 +0900

    Version updated

---
+++ TbBLTBasicSample/TbBTSDK_BLT_v110/include/TbBTSDK/TbBTPreliminary.h
@@ -0,0 +1,15 @@
+//
+//  TbBTPreliminary.h
+//  TbBTChecker
+//
+//  Created by Ueda on 2016/02/23.
+//  Copyright © 2016年 3bitter.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TbBTPreliminary : NSObject
+
++ (void)setUpWithCompletionHandler:(void (^)(BOOL success))completionHandler;
+
+@end
