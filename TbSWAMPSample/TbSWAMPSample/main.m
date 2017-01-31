2433ff11cc7929fccd4bb59743108fb8fe105b4d
tkfmueda <tkfm@3bitter.com>
2017-01-31 19:28:58 +0900

    first commit

---
+++ TbSWAMPSample/TbSWAMPSample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TbSWAMPSample
+//
+//  Created by Ueda on 2017/01/30.
+//  Copyright © 2017年 3bitter Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
