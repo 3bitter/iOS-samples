34c4ff4d9d3af858cbf3f9806ef8a3d7a6c8e31b
tkfmueda <tkfm@3bitter.com>
2017-03-02 00:24:22 +0900

    SDK updated

---
+++ TbSWAMPSample/Tb_SWAMP_iOS_SDK_v130(b2)/include/TbBTSDK/TbBTOnetimeValidationClient.h
@@ -0,0 +1,39 @@
+//
+//  TbBTOnetimeValidationClient.h
+//  TbBTSDK
+//
+//  Created by Ueda on 2016/08/09.
+//  Copyright © 2016年 3bitter.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreLocation/CoreLocation.h>
+
+@protocol TbBTOnetimeValidationClientDelegate;
+
+typedef NS_ENUM(NSInteger, TbBTOnetimeValidationError) {
+    TbBTOnetimeValidationErrorInvalidApp,
+    TbBTOnetimeValidationErrorInvalidRegion,
+    TbBTOnetimeValidationErrorInvalidBeacon,
+    TbBTOnetimeValidationErrorInternalServerError
+};
+
+
+@interface TbBTOnetimeValidationClient : NSObject<NSURLSessionDelegate, NSURLSessionTaskDelegate, NSURLSessionDataDelegate>
+
+@property (weak, nonatomic) id<TbBTOnetimeValidationClientDelegate> delegate;
+
+- (void)requestValidationForOnetimeBeacon:(CLBeacon *)beaconInfo;
+
+@end
+
+
+@protocol TbBTOnetimeValidationClientDelegate <NSObject>
+
+- (void)confirmedAsValidBeacon;
+- (void)decidedAsInvalidBeacon;
+
+// Includes Not found, Invalid beacon, Invalid app token
+- (void)didFailToValidateBeaconWithError:(NSError *)error;
+
+@end
